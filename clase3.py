b5b6bcca9f50ea9d48041e6a1caa13f33d6ccd08
David M <davamartinezrod@gmail.com>
2024-09-06 15:28:25 -0500

    agregue todos los archivos de las primeras 5 clases

---
+++ clase3.py
@@ -0,0 +1,7 @@
+saludo = "Hola"
+n2ombre_1 = "Hi"
+n2ombre_2 = "Carli"
+print(saludo)
+print(n2ombre_2)
+
+
---
+++ clase4.py
@@ -0,0 +1,9 @@
+name = 'CARLA Marcela'
+last_name = '  Florida   Roman  '
+print(name * 5)
+print(name + ' ' + last_name)
+print(len(name))
+print(len(last_name))
+print(name.upper())
+print(last_name.strip())
+
